3833dc78e88d07ff5a03f54bf40f3749d32fdf9d
Tye Knappenberger <tyeknapp.pro@gmail.com>
2019-12-06 11:56:04 -0500

    Help screen complete

--- FileLineLengthSorter.java
+++ FileLineLengthSorter.java
@@ -8,6 +8,20 @@ import java.util.ArrayList;
  */
 public class FileLineLengthSorter {
   public static void main(String[] args) {
-    File file = new File(args[0]);
+    if (args.length == 0 || args[0].equals("-h")) {
+      System.out.println("   Proper usage:\t$ java FileLineLengthSorter {-n|-o} fullFilePath [numLinesSkipped]");
+      System.out.println(
+          "----------------------------------------------------------------------------------------------------");
+      System.out.println("\t     -n:\tCreate a new file in the same location with sorted appended to the file name");
+      System.out.println("\t     -o:\tOverwrite the original file with the new sorted data");
+      System.out.println("   fullFilePath:\tThe full path to the file to be sorted");
+      System.out.println("numLinesSkipped:\tNumber of lines for the unsorted header. Optional");
+      System.out.println(
+          "----------------------------------------------------------------------------------------------------");
+      System.out.println("Type:  $java FileLineLengthSorter -h [...]^  to bring up this screen");
+      System.exit(0);
+      return;
+    }
+
   }
 }
